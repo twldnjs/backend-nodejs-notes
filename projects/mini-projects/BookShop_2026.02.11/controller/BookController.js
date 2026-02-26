3ea8bc205549dbf3016dc3095f82b0a074d2817b
twldnjs <jwn032@gmail.com>
2026-02-26 22:49:46 +0900

    feat: 2/23 과제

--- projects/mini-projects/BookShop_2026.02.11/controller/BookController.js
+++ projects/mini-projects/BookShop_2026.02.11/controller/BookController.js
@@ -1,30 +1,43 @@
 const conn = require('../mariadb'); // db 모듈
 const { StatusCodes } = require('http-status-codes'); // status code 모듈
 
-// 요약된 전체 도서 조회
+// (카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-  const { category_id } = req.query;
+  const { category_id, news, limit, currentPage } = req.query;
 
-  if (category_id) {
-    let sql = 'SELECT * FROM books WHERE category_id = ?';
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      if (results.length) return res.status(StatusCodes.OK).json(results);
-      else return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    let sql = 'SELECT * FROM books';
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
+  // limit : page 당 도서 수
+  // currentPage: 현재 몇 페이지 ex 1,2,3...
+  // offset :                    0 ,3, 6, 9
+  //                             limit * (currentPage -1)
+
+  let offset = limit * (currentPage - 1);
+  let sql = 'SELECT * FROM books';
+  let values = [];
+
+  if (category_id && news) {
+    sql +=
+      'WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH) AND NOW()';
+    values = values.push(category_id, news);
+  } else if (category_id) {
+    sql += 'WHERE category_id = ?';
+    values = [category_id];
+  } else if (news) {
+    sql += 'WHERE pub_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH) AND NOW()';
+    values = [category_id];
   }
+
+  sql += 'LIMIT ? OFFSET ?';
+  values.push(parseInt(limit), offset);
+  // 카테고리 별 조회
+  sql = 'SELECT * FROM books WHERE category_id = ?';
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length) return res.status(StatusCodes.OK).json(results);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
 };
 
 // 개별 도서 조회
